3f237c1b9039bbbc7a23577732ff08a684ed93b7
aoi1 <tkhsaoi88@gmail.com>
2019-09-04 16:01:17 +0900

    added put document

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
+	"os"
 )
 
 func main() {
@@ -19,9 +20,21 @@ func main() {
 		buf.ReadFrom(res.Body)
 		fmt.Printf("Create OK: %s\n", buf.String())
 	}
+	reader, _ := os.Open("./document.json")
+	defer reader.Close()
+	if res, err := es.Index(indexName, reader); err == nil {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(res.Body)
+		fmt.Printf("PUT document OK: %s\n", buf.String())
+	}
 	if res, err := es.Cat.Indices(); err == nil {
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(res.Body)
 		fmt.Printf("Cat OK: %s\n", buf.String())
+	}
+	if res, err := es.Indices.Get([]string{indexName}); err == nil {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(res.Body)
+		fmt.Printf("Get document OK: %s\n", buf.String())
 	}
 }
