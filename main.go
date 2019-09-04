d39e1d262642150cb58ff70923fe41e3f0bf043b
aoi1 <tkhsaoi88@gmail.com>
2019-09-04 15:33:13 +0900

    add create, delete index

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
 )
 
 func main() {
 	es, _ := elasticsearch.NewDefaultClient()
-	if res, err := es.Cat.Indices(); err == nil {	
+	indexName := "sample"
+	if res, err := es.Indices.Delete([]string{indexName}); err == nil {
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(res.Body)
-		fmt.Printf("OK: %s\n", buf.String())
+		fmt.Printf("Delete OK: %s\n", buf.String())
+	}
+	if res, err := es.Indices.Create(indexName); err == nil {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(res.Body)
+		fmt.Printf("Create OK: %s\n", buf.String())
+	}
+	if res, err := es.Cat.Indices(); err == nil {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(res.Body)
+		fmt.Printf("Cat OK: %s\n", buf.String())
 	}
 }
